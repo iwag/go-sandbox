4a31e1ddd75ea0f2586253830e5ccd5d7bc1c394
iwag <iwagame@gmail.com>
2016-11-17 18:15:40 +0900

    store tinyurl as map

--- tinyurl/server.go
+++ tinyurl/server.go
@@ -3,25 +3,52 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo"
+	"crypto/md5"
+	"fmt"
+	"github.com/labstack/gommon/log"
 )
 
-func getLink(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
+type (
+	link struct {
+		Key string `json:"key"`
+		Link string `json:"link"`
+	}
+)
+
+var (
+	links map[string]link
+)
+
+func GetLink(c echo.Context) error {
+	key := c.Param("key")
+	l,e := links[key]
+	if e!=false {
+		return c.String(http.StatusOK, l.Link)
+	} else {
+		return c.String(http.StatusNotFound, "not found")
+	}
 }
 
-func createLink(c echo.Context) error {
-	id := c.FormValue("link")
-	return c.String(http.StatusOK, "unique_link:" + id) // TODO
+func CreateLink(c echo.Context) error {
+	l := c.FormValue("link")
+	key :=  fmt.Sprintf("%x", md5.Sum([]byte(l)))
+	links[key] = link{
+		Key: key,
+		Link: l,
+	}
+	log.Printf("shorten_link:" + key)
+	return c.String(http.StatusOK, "shorten_link:" + key) // TODO
 }
 
 func main() {
+	links = make(map[string]link)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Welcome to tinyurl")
 	})
-	e.POST("/create", createLink)
-	e.GET("/:id", getLink)
+	e.POST("/create", CreateLink)
+	e.GET("/:key", GetLink)
 
 	if err := e.Start(":9000"); err != nil {
 		e.Logger.Fatal(err.Error())
